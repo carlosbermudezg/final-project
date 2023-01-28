f00769d7ec4b62e27ee616ba0b8a978e77bf1c40
GHIOMA <martinezgabrielalejandro4@gmail.com>
2023-01-28 01:43:04 -0300

    GM-advances-28-01-23

--- src/store/slices/products.slice.jsx
+++ src/store/slices/products.slice.jsx
@@ -16,12 +16,23 @@ export const productSlice = createSlice({
 // el get para tener info del api
 // export el thunk en home
 // el dispatch lleva el Loader
- export const getProductsThunk =() =>  dispatch => {
+ export const getProductsThunk =() =>  (dispatch) => {
 
   dispatch(setIsLoading(true))
 
   axios
   .get(`https://e-commerce-api.academlo.tech/api/v1/products`)
+  .then(resp => dispatch(setProduct(resp.data.data.products)))
+  .catch(error => console.log(error))
+  .finally( () => dispatch(setIsLoading(false)))
+ }
+
+ export const filterCategoriesThunk =(id) =>  (dispatch) => {
+
+  dispatch(setIsLoading(true))
+
+  axios
+  .get(`https://e-commerce-api.academlo.tech/api/v1/products/?category=${id}`)
   .then(resp => dispatch(setProduct(resp.data.data.products)))
   .catch(error => console.log(error))
   .finally( () => dispatch(setIsLoading(false)))
